79a56405b6ba242b5a60d81222739b5feabc3cd0
Jake Hamilton <jacob@theleaguestock.com>
2022-01-07 18:43:01 -0600

    hang

--- components/slider.js
+++ components/slider.js
@@ -1,27 +1,29 @@
-const { iif, Observable, BehaviorSubject, AsyncSubject, Subject, interval, of , fromEvent, merge, empty, delay, from } = rxjs;
+const { iif, Observable, BehaviorSubject, AsyncSubject, Subject, interval, of, fromEvent, merge, empty, delay, from } = rxjs;
 const { throttleTime, mergeMap, switchMap, scan, take, takeWhile, map, tap, startWith, filter, mapTo } = rxjs.operators;
 
 
 export class ActionRelayer {
-  /* 
+  /*
     TODO: Develop relayer class which is
     instatiated as a data wrapper then passed
-    to other class constructor that 
+    to other class constructor that
     mediates/controls/notifies data changes
     between objects
   */
   constructor(data = [[]] || new Map() || {}) {
     this._data = new Map(
       [...(Array.isArray(data) || data instanceof Map ? [...data] : Object.entries(data))]
-      .map(([k, v], i) => {
-        return ['x', 'y', 'width', 'w', 'height', 'h', 'strokeWidth', 'id'].includes(k) ? [k, v] : [k, v];
-      })
+        .map(([k, v], i) => {
+          return ['x', 'y', 'width', 'w', 'height', 'h', 'strokeWidth', 'id'].includes(k) ? [k, v] : [k, v];
+        })
     );
   };
-  get data() { return this._data };
-  set data(newValue) { this._data = newValue };
+  get data() { return this._data; };
+  set data(newValue) { this._data = newValue; };
 }
 
+
+// TODO Add Slider RepositionX
 
 export class SliderGroup {
   constructor(parentSvg, element, config = new SliderConfig()) {
@@ -31,10 +33,11 @@ export class SliderGroup {
     this.handleRadius = this.config.handleRadius;
     this.strokeWidth = config.strokeWidth || 4;
     this.root = element || document.createElementNS(SVG_NS, 'g');
-
-    this.originY = this.height / 2 //((this.height - this.y1) - this.y) + (this.strokeWidth * 2.5)
+    this.adjusterHeight = this.config.adjusterHeight || 15;
+    this.originY = this.height / 2; //((this.height - this.y1) - this.y) + (this.strokeWidth * 2.5)
     this.changeY = this.originY;
-    this.value$ = new BehaviorSubject(this.originY)
+    this.changeX = this.config.x;
+    this.value$ = new BehaviorSubject(this.originY);
     this.value = this.originY;
 
     this.drag$;
@@ -45,6 +48,7 @@ export class SliderGroup {
       ['handleGroup', document.createElementNS(SVG_NS, 'g')],
       ['track', document.createElementNS(SVG_NS, 'line')],
       ['background', document.createElementNS(SVG_NS, 'rect')],
+      ['sliderAdjuster', document.createElementNS(SVG_NS, 'rect')],
       ['root', this.root],
     ]);
 
@@ -53,12 +57,14 @@ export class SliderGroup {
     this.track = this.getElement('track');
     this.handle = this.getElement('handle');
     this.handleGroup = this.getElement('handleGroup');
+    this.adjuster = this.getElement('sliderAdjuster');
     this.textNode = this.getElement('text');
 
-    this.trackHeight = (this.y2 - this.y1) //- this.handleRadius * 2
+    this.trackHeight = (this.y2 - this.y1); //- this.handleRadius * 2
     this.offset;
+    this.xPos = this.changeX;
     this.transform;
-    this.translate
+    this.translate;
     this.CTM;
     this.coord;
 
@@ -89,57 +95,65 @@ export class SliderGroup {
     }
   }
 
+  repositionX(evt) {
+    const { target } = evt;
+    if (target !== this.adjuster) return;
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    this.coord = this.getMousePosition(evt);
+    this.changeX = this.coord.X - this.offset.X;
+
+    this.sliderTransforms.setTranslate(this.changeX, 0);
+  }
+
   startDrag(evt) {
     evt.stopPropagation();
     this.setAttr('handle', 'fill', 'url(#activeHandleGradient)');
-
-    this.selected = evt.currentTarget
+    this.selected = evt.currentTarget;
     this.transform = this.handleTransforms.getItem(0);
-
     this.offset = this.getMousePosition(evt);
     this.offset.x -= this.transform.matrix.e;
     this.offset.y -= this.transform.matrix.f;
   }
 
   drag(evt) {
-    this.handle.classList.add('active')
+    this.handle.classList.add('active');
     if (this.selected) {
       evt.preventDefault();
       evt.stopPropagation();
       this.coord = this.getMousePosition(evt);
-      this.changeY = this.coord.y - this.offset.y
+      this.changeY = this.coord.y - this.offset.y;
 
       if (this.isInBounds) {
         this.transform.setTranslate(0, this.changeY);
-        this.textNode.textContent = Math.abs(this.value)
+        this.textNode.textContent = Math.abs(this.value);
 
-        this.handle.classList.add('at-origin')
-        this.handleRotate.setRotate(this.value * 7.2, this.width / 2, 0)
-        this.textNodeRotate.setRotate(-this.value * 7.2, this.width / 2, 0)
+        this.handle.classList.add('at-origin');
+        this.handleRotate.setRotate(this.value * 7.2, this.width / 2, 0);
+        this.textNodeRotate.setRotate(-this.value * 7.2, this.width / 2, 0);
 
-        if (Math.abs(this.value) <= 0.9) this.handle.classList.add('at-origin')
-        else this.handle.classList.remove('at-origin')
+        if (Math.abs(this.value) <= 0.9) this.handle.classList.add('at-origin');
+        else this.handle.classList.remove('at-origin');
       }
     }
   }
 
   endDrag(evt) {
-    this.selected = null
-    // this.handle.classList.remove('active')
+    this.selected = null;
     this.setAttr('handle', 'fill', 'url(#inactiveHandleGradient)');
-
   }
 
   createText() {
     const textNode = document.createElementNS(SVG_NS, "text");
     const text = document.createTextNode(this.value);
     textNode.appendChild(text);
-    return textNode
+    return textNode;
   }
 
   setAttr(childKey = '', attr = '', value) {
     this.getElement(childKey).setAttribute(attr, value);
-    return this.getElement(childKey)
+    return this.getElement(childKey);
   }
 
   getAttr(childKey = '', attr = '') { return parseInt(this.getElement(childKey).getAttributeNS(null, attr)); }
@@ -148,7 +162,7 @@ export class SliderGroup {
 
   getMousePosition(evt) {
     const CTM = this.parentSvg.getScreenCTM();
-    if (evt.touches) { evt = evt.targetTouches[0] }
+    if (evt.touches) { evt = evt.targetTouches[0]; }
     return {
       x: (evt.clientX - CTM.e) / CTM.a,
       y: (evt.clientY - CTM.f) / CTM.d
@@ -156,20 +170,31 @@ export class SliderGroup {
   }
 
   init() {
-    this.width = this.config.width
-    this.height = this.config.height
+    this.width = this.config.width;
+    this.height = this.config.height;
     this.root.classList.add('sliderGroup');
 
     this.setAttr('background', 'width', this.width);
     this.setAttr('background', 'height', this.height);
     this.setAttr('background', 'rx', '22px');
-    this.background.classList.add('slider-bg')
+    this.background.classList.add('slider-bg');
+
+
+
+    this.setAttr('sliderAdjuster', 'x', this.width / 4);
+    this.setAttr('sliderAdjuster', 'y', this.height + 5);
+    this.setAttr('sliderAdjuster', 'width', this.width / 2);
+    this.setAttr('sliderAdjuster', 'height', this.adjusterHeight);
+    this.setAttr('sliderAdjuster', 'rx', '8px');
+    this.setAttr('sliderAdjuster', 'fill', '#18181890');
+    this.setAttr('sliderAdjuster', 'stroke', '#00000050');
+    this.background.classList.add('sliderAdjuster');
 
     this.setAttr('track', 'x1', this.width / 2);
     this.setAttr('track', 'x2', this.width / 2);
-    this.setAttr('track', 'y1', this.handleRadius)
-    this.setAttr('track', 'y2', this.height - this.handleRadius)
-    this.track.classList.add('slider-track')
+    this.setAttr('track', 'y1', this.handleRadius);
+    this.setAttr('track', 'y2', this.height - this.handleRadius);
+    this.track.classList.add('slider-track');
 
     this.setAttr('handle', 'r', this.handleRadius);
     this.setAttr('handle', 'cx', this.width / 2);
@@ -179,17 +204,21 @@ export class SliderGroup {
     this.handle.classList.add('at-origin');
 
     this.setAttr('text', 'x', this.handleRadius);
-    this.setAttr('text', 'y', this.handleRadius / 4)
+    this.setAttr('text', 'y', this.handleRadius / 4);
     this.textNode.classList.add('text-node');
     this.textNode.setAttributeNS(null, 'text-anchor', 'middle');
 
-    this.handleGroup.appendChild(this.handle)
-    this.handleGroup.appendChild(this.textNode)
+    this.handleGroup.appendChild(this.handle);
+    this.handleGroup.appendChild(this.textNode);
 
-    this.root.appendChild(this.background)
-    this.root.appendChild(this.track)
-    this.root.appendChild(this.handleGroup)
-    this.parentSvg.appendChild(this.root)
+    this.root.appendChild(this.background);
+    this.root.appendChild(this.track);
+    this.root.appendChild(this.handleGroup);
+    this.root.appendChild(this.adjuster);
+    this.parentSvg.appendChild(this.root);
+
+    this.root.addEventListener('touchmove', this.repositionX.bind(this));
+
 
     this.root.addEventListener('mousedown', this.startDrag.bind(this));
     this.root.addEventListener('mousemove', this.drag.bind(this));
@@ -202,11 +231,11 @@ export class SliderGroup {
   get isInBounds() {
     return (
       this.changeY >= (this.config.y - this.config.y) + this.handleRadius - this.strokeWidth &&
-      this.changeY <= (this.config.height - this.handleRadius) + this.strokeWidth // + (this.handleRadius * 3) 
-    )
+      this.changeY <= (this.config.height - this.handleRadius) + this.strokeWidth // + (this.handleRadius * 3)
+    );
   }
 
-  set value(val) { this._value = val }
+  set value(val) { this._value = val; }
   get value() {
     const origin = this.config.height / 2;
     let val;
@@ -214,26 +243,26 @@ export class SliderGroup {
       val = Math.round((((this.changeY - origin)) / (this.config.height - (origin + this.handleRadius))) * 100);
     } else if (this.changeY > origin) {
       val = Math.round(((this.changeY - origin) / (this.height - (origin + this.handleRadius))) * 100);
-    } else val = 0
-    val = val > 100 ? 100 : val
+    } else val = 0;
+    val = val > 100 ? 100 : val;
     this.value$.next(val);
-    return val
+    return val;
   }
 
   get children() {
-    return this._children
+    return this._children;
   };
-  set children(newValue) { this._children = newValue };
-  get trackRange() { return (this.y2 - this.y1) || 0 }
+  set children(newValue) { this._children = newValue; };
+  get trackRange() { return (this.y2 - this.y1) || 0; }
   get center() {
     return {
       x: this.x + (this.width / 2),
       y: this.y + (this.height / 2),
-    }
+    };
   }
-  get y1() { return (parseInt(this.track !== undefined ? +this.track.getAttribute('y1') : this.config.y + this.handleRadius)) }
+  get y1() { return (parseInt(this.track !== undefined ? +this.track.getAttribute('y1') : this.config.y + this.handleRadius)); }
 
-  get y2() { return (parseInt(this.track !== undefined ? +this.track.getAttribute('y2') : this.config.height - this.handleRadius)) }
+  get y2() { return (parseInt(this.track !== undefined ? +this.track.getAttribute('y2') : this.config.height - this.handleRadius)); }
   // get y2() { return (parseInt(this.track !== undefined ? +this.track.getAttribute('y2') : this.config.height)) }
   // set y2(newValue) {
   //   // this.track.setAttribute('y2', newValue) // - this.strokeWidth)
@@ -264,20 +293,25 @@ export class SliderGroup {
   //   this.root.setAttribute('y', newValue)
   //   this.background.setAttribute('y', newValue)
   // }
-  get width() { return (parseInt(this.background !== undefined ? this.background.getAttribute('width') : this.config.width)) }
+  get width() { return (parseInt(this.background !== undefined ? this.background.getAttribute('width') : this.config.width)); }
   set width(newValue) {
-    this.root.setAttribute('width', newValue)
-    this.background.setAttribute('width', newValue)
+    this.root.setAttribute('width', newValue);
+    this.background.setAttribute('width', newValue);
   }
-  get height() { return (parseInt(this.background !== undefined ? +this.background.getAttribute('height') : this.config.height)) }
+  get height() { return (parseInt(this.background !== undefined ? +this.background.getAttribute('height') : this.config.height)); }
   set height(newValue) {
-    this.root.setAttribute('height', newValue)
-    this.background.setAttribute('height', newValue)
+    this.root.setAttribute('height', newValue);
+    this.background.setAttribute('height', newValue);
+  }
+  get extendedHeight() { return (parseInt(this.background !== undefined ? +this.background.getAttribute('height') : this.config.height) + this.adjusterHeight); }
+  set extendedHeight(newValue) {
+    this.root.setAttribute('height', newValue);
+    this.background.setAttribute('height', newValue);
   }
 }
 
 
 {
   SliderGroup,
-  ActionRelayer
+    ActionRelayer;
 }
--- scratch/app.js
+++ scratch/app.js
@@ -1,4 +1,4 @@
-const { iif, ReplaySubject, AsyncSubject, Subject, interval, of , fromEvent, merge, empty, delay, from } = rxjs;
+const { iif, ReplaySubject, AsyncSubject, Subject, interval, of, fromEvent, merge, empty, delay, from } = rxjs;
 const { throttleTime, mergeMap, switchMap, scan, take, takeWhile, map, tap, startWith, filter, mapTo } = rxjs.operators;
 
 
@@ -7,17 +7,17 @@ import { Paddle } from '../components/paddle.js';
 
 export class Game {
   constructor(selector, sliderConfig) {
-    this.root = document.querySelector(selector)
+    this.root = document.querySelector(selector);
     this.sliders = sliderConfig
-      .reduce((acc, curr) => ({ ...acc, [curr.id]: new SliderGroup(this.root, undefined, curr) }), {})
-   
-    this.paddleLeft = new Paddle(this.root, this.root.querySelector('#leftPaddleGroup'), { input$: this.sliders.left.value$, height: 100, boardHeight: 425, width: 25, })
-    this.paddleRight = new Paddle(this.root, this.root.querySelector('#rightPaddleGroup'), { input$: this.sliders.right.value$, height: 100, boardHeight: 425, width: 25, })
+      .reduce((acc, curr) => ({ ...acc, [curr.id]: new SliderGroup(this.root, undefined, curr) }), {});
+
+    this.paddleLeft = new Paddle(this.root, this.root.querySelector('#leftPaddleGroup'), { input$: this.sliders.left.value$, height: 100, boardHeight: 425, width: 25, });
+    this.paddleRight = new Paddle(this.root, this.root.querySelector('#rightPaddleGroup'), { input$: this.sliders.right.value$, height: 100, boardHeight: 425, width: 25, });
 
     console.log('game', this);
   };
-  get prop() { return this._prop };
-  set prop(newValue) { this._prop = newValue };
+  get prop() { return this._prop; };
+  set prop(newValue) { this._prop = newValue; };
 }
 
 const sliderConfig = [
@@ -25,7 +25,7 @@ const sliderConfig = [
     x: 50,
     y: 450,
     width: 50,
-    height: 250,
+    height: 225,
     handleRadius: 25,
     strokeWidth: 1,
     id: 'left',
@@ -34,12 +34,12 @@ const sliderConfig = [
     x: 285,
     y: 450,
     width: 50,
-    height: 250,
+    height: 225,
     handleRadius: 25,
     strokeWidth: 1,
     id: 'right',
   },
-]
+];
 
 // const relayer = new ActionRelayer(sliderConfig)
 // console.log('relayer', [...relayer])
